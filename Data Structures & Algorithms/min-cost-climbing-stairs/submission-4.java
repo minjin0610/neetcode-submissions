0b4d8b63e8d08109f9290d512ba0bd03ccf394f7
minjin0610 <138571857+minjin0610@users.noreply.github.com>
2026-08-20 22:15:58 +0900

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        dp[1] = cost[0];
+        dp[2] = Math.min(cost[0]+cost[1], cost[1]);
+
+        for (int i=3; i<=n; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i-1];
+        }
+
+        return Math.min(dp[n], dp[n-1]);
+    }
+}
+
+// cost 배열, cost[i]는 i번쨰 계단까지 올라가는데 드는 비용
+// 비용을 지붎하면 (i + 1)th floor or the (i + 2)th floor로 갈 수 있음
+// 시작을 인덱스 0 또는 1에서 할 수 있음
+// 최소 비용을 골라라
